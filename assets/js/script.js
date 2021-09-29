053caa8110978bb8f5b78d98665fcaa44dff9cd2
David Abbey <davidabbey@earthlink.net>
2021-09-28 20:15:26 -0700

    evening commit. time slots added in html. store and pull in js working, starting how to assess time of day control for colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
-var display3 = $('.display-3')
-var lead = $(".lead")
 var currentDay = $("#currentDay")
-var container = $(".container")
-var todaysDate = moment().format('dddd, MMM Do YYYY')
+var todaysDate = moment().format('dddd, MMM Do YYYY, HH:mm')
+var saveBtn = $(".saveBtn")
+
+//Adds current date to jumbotron
+currentDay.text(todaysDate)
+
+//Controls coloring of appt fields based on past, current, or upcoming
+var momentTime = moment().startOf()
+var hour = moment().format("H")
+
+for (var i = 8; i < 18; i++) {
+    
+}
+
+//Save each field to local storage when pressed, if user hasn't entered text alert telling them nothing to save, though a save key for that time is still created
+saveBtn.on("click", function() {
+    
+    var time = $(this).parent().attr("id")
+    var appointment = $(this).siblings(".todo").val()
+    localStorage.setItem(time, appointment)
+
+    if (appointment == "") {
+        alert("You have no appointments to save during that time.")
+    }
+})
+
+//Fills in saved appt's on load
+for (var i = 9; i < 18; i++) {
+    $(`.appt-${i}`).val(localStorage.getItem(`hour-${i}`))
+}
+
+
