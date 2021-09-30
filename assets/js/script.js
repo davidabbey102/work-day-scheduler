0bfcacd687d1743f41dfb10559723292ec7a9325
David Abbey <davidabbey@earthlink.net>
2021-09-29 17:18:43 -0700

    created reoccuring hour field in java to reproduce needed number of times after repeated tries of append and other methods kept giving multiple re-occurences of the scheduler so removed from html. coloring works, local still functional from yesterday. added interval to jumbotron time to count seconds up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,50 @@
-var currentDay = $("#currentDay")
-var todaysDate = moment().format('dddd, MMM Do YYYY, HH:mm')
-var saveBtn = $(".saveBtn")
-
 //Adds current date to jumbotron
-currentDay.text(todaysDate)
-
-//Controls coloring of appt fields based on past, current, or upcoming
-var momentTime = moment().startOf()
-var hour = moment().format("H")
-
-for (var i = 8; i < 18; i++) {
-    
+setInterval(timer, 1000)
+function timer() {
+    var currentDay = $("#currentDay")
+    var todaysDate = moment().format('dddd, MMM Do YYYY, HH:mm:ss')
+    currentDay.text(todaysDate)
 }
 
-//Save each field to local storage when pressed, if user hasn't entered text alert telling them nothing to save, though a save key for that time is still created
-saveBtn.on("click", function() {
+//Controls coloring of appt fields based on past, current, or upcoming
+var momentTime = moment().startOf('day').add(8, 'hour')
+var hour = moment().format("H")
+
+for (var i = 9; i < 18; i++) {
+    var hourField = momentTime.add(1, 'hour').format('HH:mm')
+    var colorCode;
     
+    if (hour > i) {
+        colorCode = 'past'
+    } else if (hour == i) {
+        colorCode = 'present'
+    } else if (hour < i) {
+        colorCode = 'future'
+    }
+
+    var populateContainer = 
+        `<div class="row" id="hour-${i}">
+            <p class="col-1 hour">${hourField}</p>
+            <textarea class="todo col-10 ${colorCode} hour-${i}"></textarea>
+            <button class="col-1 saveBtn"><i class="fa fa-save fa-w-14 fa-2x"></i></button>
+        </div>`
+    
+    $('.container').append(populateContainer)
+}
+
+var saveBtn = $(".saveBtn")
+
+//Save each related textarea text to local storage when pressed
+saveBtn.on("click", function(event) {
     var time = $(this).parent().attr("id")
     var appointment = $(this).siblings(".todo").val()
     localStorage.setItem(time, appointment)
-
-    if (appointment == "") {
-        alert("You have no appointments to save during that time.")
-    }
+    console.log(time, appointment)
 })
 
 //Fills in saved appt's on load
 for (var i = 9; i < 18; i++) {
-    $(`.appt-${i}`).val(localStorage.getItem(`hour-${i}`))
+    $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`))
 }
 
 
